d6f4926eeabfef6c3b2ae4b2c11594400db36f02
Lucas <lucascsilva.mg@diariosassociados.com.br>
2021-12-28 01:17:21 -0300

    Chapter IV - autenticação e autorização

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,120 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import Router from 'next/router';
+import { setCookie, parseCookies, destroyCookie } from 'nookies';
+
+import { api } from '../services/apiClient';
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signOut: () => void;
+  isAuthenticated: boolean;
+  user: User;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+let authChanel: BroadcastChannel;
+
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token');
+  destroyCookie(undefined, 'nextauth.refreshToken');
+
+  authChanel.postMessage('signOut');
+
+  Router.push('/');
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  useEffect(() => {
+    authChanel = new BroadcastChannel('auth');
+
+    authChanel.onmessage = (message) => {
+      switch (message.data) {
+        case 'signOut':
+          signOut();
+          authChanel.close();
+          break;
+        // case 'signIn':
+        //   Router.push('/dashboard');
+        //   console.log('sig');
+        //   break;
+        default:
+          break;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+
+    if (token) {
+      api
+        .get('/me')
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
+
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password,
+      });
+
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/',
+      });
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push('/dashboard');
+      // authChanel.postMessage('signIn');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ src/services/api.ts
@@ -0,0 +1,107 @@
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../context/AuthContext';
+import { AuthTokenError } from './errors/AuthTokenError';
+
+let isRefreshing = false;
+let failedRequestsQueue = [];
+
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: 'http://localhost:3333',
+    headers: {
+      Authorization: `Bearer ${cookies['nextauth.token']}`,
+    },
+  });
+
+  api.interceptors.response.use(
+    (responseSuccess) => {
+      return responseSuccess;
+    },
+    (responseError: AxiosError) => {
+      if (responseError.response.status === 401) {
+        if (responseError.response.data?.code === 'token.expired') {
+          // renovar token
+          cookies = parseCookies(ctx);
+
+          const { 'nextauth.refreshToken': refreshToken } = cookies;
+          const originalConfig = responseError.config;
+
+          if (!isRefreshing) {
+            isRefreshing = true;
+
+            api
+              .post('/refresh', {
+                refreshToken,
+              })
+              .then((response) => {
+                const { token } = response.data;
+
+                setCookie(ctx, 'nextauth.token', token, {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: '/',
+                });
+
+                setCookie(
+                  ctx,
+                  'nextauth.refreshToken',
+                  response.data.refreshToken,
+                  {
+                    maxAge: 60 * 60 * 24 * 30, // 30 days
+                    path: '/',
+                  }
+                );
+
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+
+                failedRequestsQueue.forEach((request) =>
+                  request.onSuccess(token)
+                );
+
+                failedRequestsQueue = [];
+              })
+              .catch((error) => {
+                failedRequestsQueue.forEach((request) =>
+                  request.onFailure(error)
+                );
+                failedRequestsQueue = [];
+
+                if (process.browser) {
+                  signOut();
+                }
+              })
+              .finally(() => {
+                isRefreshing = false;
+              });
+          }
+
+          return new Promise((resolve, reject) => {
+            failedRequestsQueue.push({
+              onSuccess: (token: string) => {
+                originalConfig.headers['Authorization'] = `Bearer ${token}`;
+
+                resolve(api(originalConfig));
+              },
+              onFailure: (error: AxiosError) => {
+                reject(error);
+              },
+            });
+          });
+        } else {
+          // deslogar usuário
+          if (process.browser) {
+            signOut();
+          } else {
+            return Promise.reject(new AuthTokenError());
+          }
+        }
+      }
+
+      return Promise.reject(responseError);
+    }
+  );
+
+  return api;
+}
